bf213686351cdaa65365c65dbfdb8bfc527e0e5a
tranvanson5 <tranvanson231099@gmail.com>
2023-09-01 11:43:02 +0700

    up

--- backend/src/main/java/com/example/demo/user/service/userManager/UserManagerService.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.user.service.userManager;
-
-import org.springframework.http.ResponseEntity;
-
-public interface UserManagerService {
-    ResponseEntity<?> getDataListUser();
-}
