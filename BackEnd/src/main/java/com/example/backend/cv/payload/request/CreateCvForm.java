51f916cc78eb632c5766a9a4ad62598c6294cbe2
thangdtu <thnagtran@gmail.com>
2023-11-27 17:47:46 +0700

    error backend

--- BackEnd/src/main/java/com/example/backend/cv/payload/request/CreateCvForm.java
+++ BackEnd/src/main/java/com/example/backend/cv/payload/request/CreateCvForm.java
@@ -14,30 +14,6 @@ import java.util.Set;
 @Data
 public class CreateCvForm {
     private String id;
-    private String name;
-    private String goal;
-    private String imageUrl;
-    //profile
-    @ManyToMany
-    private Set<Profile> profiles = new HashSet<>();
-    //education
-    private Set<Education> educations = new HashSet<>();
-    //    project
-    private Set<Project> projects = new HashSet<>();
-    //    skills
-    private Set<Skill> skills = new HashSet<>();
-    //    experience
-    private Set<Experience> experiences = new HashSet<>();
-    //    certification
-    private Set<Certification> certifications = new HashSet<>();
-    //    certification
-    private Set<Award> awards = new HashSet<>();
-    //    active
-    private Set<Active> active = new HashSet<>();
-    //    interests
-    private Set<Interests> interests = new HashSet<>();
-    //    references
-    private Set<Reference> references = new HashSet<>();
-    //    Addition information
-    private Set<AddInformation> addInformations = new HashSet<>();
+    private String content;
+
 }
